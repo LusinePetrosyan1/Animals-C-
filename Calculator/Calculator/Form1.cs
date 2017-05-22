0e5db0e7e9e5d09cf7d49d27f4722dfd47c5e641
user <user@DESKTOP-TP634G3>
2017-05-22 17:56:27 +0400

    change 5

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -14,7 +14,7 @@ namespace Calculator
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent() ;
         }
         private void button1_Click(object sender, EventArgs e)
         {
