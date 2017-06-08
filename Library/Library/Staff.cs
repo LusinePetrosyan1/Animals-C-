011711e241fb282771f032935373943cf97f01aa
user <user@DESKTOP-TP634G3>
2017-06-09 02:10:32 +0400

    change

---
+++ Library/Library/Staff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Staff:User
+    {
+        public Staff(string name, string surname, string login, string password, decimal money) : base(name, surname, login, password, money)
+        {
+        }
+    }
+}
