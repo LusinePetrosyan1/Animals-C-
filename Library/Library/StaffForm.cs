71780d8b857c24006e75e70674f6163a792045cb
Xch <khachatur1998@mail.ru>
2017-06-10 21:54:51 +0400

    gfhg

--- Library/Library/StaffForm.cs
+++ Library/Library/StaffForm.cs
@@ -12,12 +12,21 @@ namespace Library
 {
     public partial class StaffForm : Form
     {
+        public User user;
         public StaffForm()
         {
             InitializeComponent();
         }
-
+        public StaffForm(User user)
+        {
+            InitializeComponent();
+        }
         private void StaffForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
