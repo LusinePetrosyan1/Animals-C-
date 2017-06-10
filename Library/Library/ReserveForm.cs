1f00a3e252237a5b5df99b41dc50c36a3d36a39b
user <user@DESKTOP-TP634G3>
2017-06-10 21:16:43 +0400

    hh

---
+++ Library/Library/ReserveForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class ReserveForm : Form
+    {
+        Book book4;
+        User user4;
+        public ReserveForm()
+        {
+            InitializeComponent();
+        }
+        public ReserveForm(Book book, User user)
+        {
+            book4 = book;
+            user4 = user;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            decimal price = book4.BookSample.Cost * int.Parse(textBox1.Text);
+            label3.Text = price + "";
+        }
+    }
+}
