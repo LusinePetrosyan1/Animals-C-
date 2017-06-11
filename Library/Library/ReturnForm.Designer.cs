be05102fdec99b496d41ef91a9fa6b19fee97c7b
Xch <khachatur1998@mail.ru>
2017-06-11 18:26:16 +0400

    Revert "sdfsd"
    This reverts commit 3bd293fe494ebcd0ec4f1ee320203c72ec647ebd.

--- Library/Library/ReturnForm.Designer.cs
+++ Library/Library/ReturnForm.Designer.cs
@@ -64,7 +64,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(116, 285);
+            this.button1.Location = new System.Drawing.Point(116, 286);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(142, 34);
             this.button1.TabIndex = 1;
